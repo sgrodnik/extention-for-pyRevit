0eafe4249f24854e8e2484e39025183846611192
fazacz <48551849+fazacz@users.noreply.github.com>
2019-06-27 15:25:54 +0300

    1

---
+++ HVAC.panel/Pritochka.pushbutton/Pritochka_script.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Otverstie"""
+__title__ = 'Выбрать приток'
+__author__ = 'SG'
+
+import clr
+clr.AddReference('System.Core')
+from System.Collections.Generic import *
+
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction, TransactionGroup, BuiltInParameter, ElementId
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+
+t = Transaction(doc, 'Выбрать\nприток')
+t.Start()
+
+k = 304.8
+
+ducts = FilteredElementCollector(doc)\
+.OfCategory(BuiltInCategory.OST_DuctCurves)\
+.WhereElementIsNotElementType().ToElements()
+
+flexDucts = FilteredElementCollector(doc)\
+.OfCategory(BuiltInCategory.OST_FlexDuctCurves)\
+.WhereElementIsNotElementType().ToElements()
+
+ductFittings = FilteredElementCollector(doc)\
+.OfCategory(BuiltInCategory.OST_DuctFitting)\
+.WhereElementIsNotElementType().ToElements()
+
+ductAccessories = FilteredElementCollector(doc)\
+.OfCategory(BuiltInCategory.OST_DuctAccessory)\
+.WhereElementIsNotElementType().ToElements()
+
+lst = list(ducts) + list(flexDucts) + list(ductFittings) + list(ductAccessories)
+
+result = []
+for el in lst:
+	if el.LookupParameter('Классификация систем').AsString() == 'Приточный воздух':
+		result.append(el.Id)
+
+uidoc.Selection.SetElementIds(List[ElementId](result))
+
+t.Commit()
---
+++ Misc.panel/Numberator.pushbutton/Numberator_script.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Нумератор пространств"""
+__title__ = 'Нумеровать\nпространство'
+__author__ = 'SG'
+import clr
+clr.AddReference('System.Core')
+from System.Collections.Generic import *
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction, TransactionGroup, BuiltInParameter, ElementId
+import sys
+from Autodesk.Revit.UI.Selection import ObjectType, ISelectionFilter
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+
+# tags = [doc.GetElement(id) for id in uidoc.Selection.GetElementIds() if isinstance(doc.GetElement(id), IndependentTag)]
+
+# if not tags:
+#     sys.exit()
+
+
+class CustomISelectionFilter(ISelectionFilter):
+    def __init__(self, nom_categorie):
+        self.nom_categorie = nom_categorie
+
+    def AllowElement(self, e):
+        if self.nom_categorie in e.Category.Name:
+            return True
+        else:
+            return False
+
+    def AllowReference(self, ref, point):
+        return true
+
+
+i = 1
+try:
+    target = uidoc.Selection.PickObject(ObjectType.Element, CustomISelectionFilter("Пространства"), 'Выберите пространство')
+    target = doc.GetElement(target.ElementId)
+    i = int(target.LookupParameter('Номер').AsString()) + 1
+except:  # Exceptions.OperationCanceledException:
+    pass
+
+t = Transaction(doc, 'Перенести марки')
+t.Start()
+
+while 1:
+    try:
+        target = uidoc.Selection.PickObject(ObjectType.Element, CustomISelectionFilter("Пространства"), 'Выберите пространство')
+    except:  # Exceptions.OperationCanceledException:
+        # t.Commit()
+        break
+        # sys.exit()
+
+    target = doc.GetElement(target.ElementId)
+    target.LookupParameter('Номер').Set(str(i))
+    i += 1
+
+
+
+t.Commit()
